c8cc6383514451198ba0f1ec9defb41cd8414210
Rafael <rafaelmaltez89@gmail.com>
2020-10-28 11:59:26 -0300

    realizado exercicio 3

--- bloco_4/dia_3/exercicio-2.js
+++ bloco_4/dia_3/exercicio-2.js
@@ -1,10 +1,10 @@
+//2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base
+
 let n = 5;
 let linha = "";
 let asterisco = "*";
 
-
-
-for (let i =0; i < 5; i++) {
+for (let i =0; i < n; i++) {
     linha += asterisco;
     console.log(linha);
 }
