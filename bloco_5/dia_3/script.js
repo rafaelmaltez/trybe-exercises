e8778e300fb0a757253b562a8d5b757f2edbefaa
Rafael <rafaelmaltez89@gmail.com>
2020-11-07 12:31:54 -0300

    reaslizado exercicio 3

--- bloco_5/dia_3/script.js
+++ bloco_5/dia_3/script.js
@@ -6,65 +6,38 @@ function createDaysOfTheWeek() {
       const days = weekDays[index];
       const dayListItem = document.createElement('li');
       dayListItem.innerHTML = days;
-  
       weekDaysList.appendChild(dayListItem);
     };
   };
-  
   createDaysOfTheWeek();
 
-
-
-
-
-  /*
-  
-  //Exercicio 1
-  function createDaysOfTheMonth() {
-    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-    const monthDaysList = document.getElementById('days');
-    for (let index =0; index < dezDaysList.length; index++) {
-        const monthDay = dezDaysList[index];      
-        const monthDayItem = document.createElement('li');
-        monthDayItem.className = 'day';        
-        monthDayItem.innerHTML = monthDay;
-        monthDaysList.appendChild(monthDayItem);
-       if(monthDayItem.innerHTML == 24 || monthDayItem.innerHTML == 25 || monthDayItem.innerHTML == 31){
-            monthDayItem.className = 'day holiday';
-        }
-        if(monthDayItem.innerHTML == 24 || monthDayItem.innerHTML == 25 || monthDayItem.innerHTML == 31){
-          monthDaysList.appendChild(monthDayItem);
-    }
+function createDaysOfTheMonth() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const monthDaysList = document.getElementById('days');
+  for (index = 0; index < dezDaysList.length; index ++){
+    const monthDayItem = document.createElement('li');
+    monthDaysList.appendChild(monthDayItem);
+    dayOfTheMonth = dezDaysList[index];        
+    monthDayItem.innerHTML = dayOfTheMonth;    
     
-  }  
-  createDaysOfTheMonth();
-
-  //Exercicio 2
-  function criaBotao(nomeDoBotao){
-      const button = document.createElement('button');
-      button.innerHTML = nomeDoBotao;
-      button.id = "btn-holiday";
-      const btnsContainer = document.querySelector('.buttons-container');
-      btnsContainer.appendChild(button);      
-  }
-  criaBotao("Feriados");
-
-  
-  //Exercicio 3
-  function coloreFeriados(){
-    let button = document.getElementById('btn-holiday');
-    button.addEventListener('click', mudaCor)
-    function mudaCor(){
-      let feriados = document.getElementsByClassName('holiday');
-      for (let index=0; index < feriados.length; index +=){
-        if(feriados.style.backgroundColor= 'rgb(238,238,238)'){
-          feriados.style.backgroundColor = 'yellow';
-        }else {
-          feriados.style.backgroundColor = 'rgb(238,238,238)';
-        }
-      }
-  }
+    if(monthDayItem.innerHTML == 24 || monthDayItem.innerHTML == 31){
+      monthDayItem.className = 'day holiday';
+    } else if(monthDayItem.innerHTML == 4 || monthDayItem.innerHTML == 11 || monthDayItem.innerHTML == 18){
+      monthDayItem.className = 'day friday';
+    } else if (monthDayItem.innerHTML == 25){
+      monthDayItem.className = 'day friday holiday';
+    } else {
+      monthDayItem.className = 'day';
+    }
+  } 
 }
-function coloreFeriados();
+createDaysOfTheMonth();
 
-*/
+function criaBotao(nomeDoBotao){
+  let button = document.createElement('button');
+  button.id = 'btn-holiday';
+  let divContainer = document.getElementsByClassName('buttons-container');
+  divContainer[0].appendChild(button);
+
+}
+criaBotao('Feriados');
