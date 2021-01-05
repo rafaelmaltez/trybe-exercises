97ee108f51be5ba8d926255ed8fc71a3f28483b0
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-05 19:19:36 -0300

    exercise 1 test number 4

--- bloco_10/dia_1/exercise1.test.js
+++ bloco_10/dia_1/exercise1.test.js
@@ -13,5 +13,10 @@ test('sum(0, 0) returns 0', () => {
 
 // Teste se a função sum lança um erro quando os parametros são 4 e "5" (string 5)
 test ('sum(4, "5") returns an error', () => {
-    expect(sum(4, "5")).toThrow();
+    expect(() => {sum(4, "5") }).toThrow()
+});
+
+// Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+test('sum(4, "5") throws error message: "parameters must be numbers"', () => {
+    expect(() => {sum(4, "5") }).toThrowError( new Error('parameters must be numbers'));
 });
