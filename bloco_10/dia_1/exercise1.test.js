cf59e83a24eecf9a9c1f2e6a248db48fa6857a2f
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-05 19:11:17 -0300

    added exercises comment descriptions

--- bloco_10/dia_1/exercise1.test.js
+++ bloco_10/dia_1/exercise1.test.js
@@ -1,11 +1,17 @@
 const { test, expect } = require('@jest/globals');
 const sum = require('./exercise1.js');
 
+//Teste se o retorno de sum(4, 5) é 9
 test('sum(4, 5) returns 9', () => {
     expect(sum(4, 5)).toBe(9);
 });
 
-
+// Teste se o retorno de sum(0, 0) é 0
 test('sum(0, 0) returns 0', () => {
     expect(sum(0, 0)).toBe(0);
+});
+
+// Teste se a função sum lança um erro quando os parametros são 4 e "5" (string 5)
+test ('sum(4, "5") returns an error', () => {
+    expect(sum(4, "5")).toThrow();
 });
