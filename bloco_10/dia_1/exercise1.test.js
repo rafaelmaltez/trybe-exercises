2c8fd691bfba91b96211d74a30f1f69cccf6400e
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-05 19:08:26 -0300

    exercise 1 test number 1

--- bloco_10/dia_1/exercise1.test.js
+++ bloco_10/dia_1/exercise1.test.js
@@ -4,3 +4,8 @@ const sum = require('./exercise1.js');
 test('sum(4, 5) returns 9', () => {
     expect(sum(4, 5)).toBe(9);
 });
+
+
+test('sum(0, 0) returns 0', () => {
+    expect(sum(0, 0)).toBe(0);
+});
