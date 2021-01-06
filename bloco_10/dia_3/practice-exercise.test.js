9c2fd68f6aed35b5ed0a94f7c70ba4731cd631f3
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-06 15:55:06 -0300

    added exercise description to practice-exercise.test.js

--- bloco_10/dia_3/practice-exercise.test.js
+++ bloco_10/dia_3/practice-exercise.test.js
@@ -1,6 +1,7 @@
 const { test, expect } = require('@jest/globals');
 const practice = require('./practice-exercise');
 
+//Teste que, quando a randomNumber() retorna um número par e isDivisible() recebe um dois, o retorno é true .
 test('random number returns even and isDivisible receives 2, return is true', () => {
     practice.randomNumber = jest
         .fn()
@@ -9,6 +10,7 @@ test('random number returns even and isDivisible receives 2, return is true', ()
     expect(practice.isDivisible(2)).toBeTruthy;
 });
 
+//escreva um teste que verifica que a função randomNumber() é chamada quando invocamos a isDivisible() 
 test('randomNumber is invoked when invoking isDivisible', () => {
     practice.isDivisible = jest.fn();
 
