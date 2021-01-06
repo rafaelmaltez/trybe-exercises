f770cbb172d73f882327dcde025e9408e671382a
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-06 15:53:24 -0300

    practice exercise 2

--- bloco_10/dia_3/practice-exercise.test.js
+++ bloco_10/dia_3/practice-exercise.test.js
@@ -1,3 +1,4 @@
+const { test, expect } = require('@jest/globals');
 const practice = require('./practice-exercise');
 
 test('random number returns even and isDivisible receives 2, return is true', () => {
@@ -5,5 +6,12 @@ test('random number returns even and isDivisible receives 2, return is true', ()
         .fn()
         .mockReturnValue(50)
     
-        expect(practice.isDivisible(2)).toBeTruthy;
-})
+    expect(practice.isDivisible(2)).toBeTruthy;
+});
+
+test('randomNumber is invoked when invoking isDivisible', () => {
+    practice.isDivisible = jest.fn();
+
+    practice.isDivisible();
+    expect(practice.randomNumber).toHaveBeenCalled;
+});
