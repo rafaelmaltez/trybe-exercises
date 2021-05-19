057fed5634b4d9ade40311cf87e64d99c265ca81
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-05-19 18:34:17 -0300

    exercicio 3

--- bloco_26/dia_4/exercises/index.js
+++ bloco_26/dia_4/exercises/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const ageMiddleware = (req, res) => {
+  req.body.age > 17 ?
+    res.status(200).send({ "message": `Hello, ${req.body.name}` }) :
+    res.status(401).send({ "message": "Unauthorized" });
+}
+
 app.use(express.json());
 
 app.get('/ping', (req, res) => {
@@ -13,6 +19,8 @@ app.post('/hello', (req, res) => {
   res.status(200);
   res.send({ "message": `Hello, ${req.body.name}` })
 });
+
+app.post('/greetings', ageMiddleware);
 
 
 app.listen(3000, () => console.log('Rodando na porta 3000'));
