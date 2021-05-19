94c7c8612253d658c3421336dcbbfe7c773a8608
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-05-19 18:53:12 -0300

    exercicios.. 7

--- bloco_26/dia_4/exercises/index.js
+++ bloco_26/dia_4/exercises/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
+const simpsons = require('./simpsons.json');
 
 const app = express();
+app.use(express.json());
 
 const ageMiddleware = (req, res) => {
   req.body.age > 17 ?
     res.status(200).send({ "message": `Hello, ${req.body.name}` }) :
     res.status(401).send({ "message": "Unauthorized" });
 }
-
-app.use(express.json());
 
 app.get('/ping', (req, res) => {
   res.status(200);
@@ -21,6 +21,20 @@ app.post('/hello', (req, res) => {
 });
 
 app.post('/greetings', ageMiddleware);
+
+app.put('/users/:name/:age', (req, res) => {
+  res.status(200).send({ "message": `Seu nome é ${req.params.name} e você tem ${req.params.age} anos de idade` })
+});
+
+app.get('/simpsons', (req, res) => {
+  res.status(200).send(simpsons);
+});
+
+app.get('/simpsons/:id', (req, res) => {
+  const simpson = simpsons.filter(simpson => simpson.id === req.params.id)[0];
+  if (!simpson) return res.status(404).send({ "message": "Simpson not found" });
+  res.status(200).send(simpson);
+});
 
 
 app.listen(3000, () => console.log('Rodando na porta 3000'));
