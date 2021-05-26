a62160a0184b81c05f450996583e5a84c272d782
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-05-26 18:54:38 -0300

    cria primeiro model e rota para requisiçao de todos os autores

---
+++ bloco_27/dia_1/model-example/models/Author.js
@@ -0,0 +1,33 @@
+const connection = require('./connection');
+
+const getNewAuthor = (authorData) => {
+  const { id, firstName, middleName, lastName } = authorData;
+
+  const fullName = [firstName, middleName, lastName].filter((name) => name).join(' ');
+
+  return {
+    id,
+    firstName,
+    middleName,
+    lastName,
+    name: fullName
+  }
+}
+
+const convertFields = (authorData) => ({
+  id: authorData.id,
+  firstName: authorData.first_name,
+  middleName: authorData.middle_name,
+  lastName: authorData.lastName
+});
+
+const getAll = async () => {
+  const [authors] = await connection.execute(
+    'SELECT id, first_name, middle_name, last_name FROM model_example.authors;'
+  );
+  return authors.map(convertFields).map(getNewAuthor);
+}
+
+module.exports = {
+  getAll,
+}
